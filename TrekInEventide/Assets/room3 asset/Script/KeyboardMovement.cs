c4ed8a6614a73ded15e550218730eb6c8540f880
wanerlllll <liw497@newschool.edu>
2024-04-14 19:51:54 -0400

    dialogue

---
+++ TrekInEventide/Assets/room3 asset/Script/KeyboardMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput) * speed * Time.deltaTime;
+
+        transform.Translate(movement);
+
+        if (DialogueManager.Instance.isDialogueActive)
+        {
+            horizontalInput = 0f;
+            verticalInput = 0f;
+        }
+    }
+}
