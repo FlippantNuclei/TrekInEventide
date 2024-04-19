7dae2ffff2253b68c44ffeccd3aed1a3b6baa1a3
Luke Kim <kiml803@newschool.edu>
2024-04-19 13:37:30 -0400

    Some Scripting

--- TrekInEventide/Assets/Room 2 Assets/Room 2 Scripts/TriggerProjector.cs
+++ TrekInEventide/Assets/Room 2 Assets/Room 2 Scripts/TriggerProjector.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class TriggerProjector : MonoBehaviour
 {
 
+    [Header("Projector Script")] 
     public ProjectorCycle projectorCycle;
+
+   // [Header("Film")] 
 
     void Update()
     {
