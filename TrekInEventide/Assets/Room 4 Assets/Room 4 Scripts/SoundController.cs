ea047b7ae3cbcecea005aa000244b13f54494e99
Luke Kim <kiml803@newschool.edu>
2024-05-10 18:04:15 -0400

    Room 4 Train announcer

---
+++ TrekInEventide/Assets/Room 4 Assets/Room 4 Scripts/SoundController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public int timeTillDeparture;
+    public int speaker;
+    [Header("Sound")]
+    public AudioSource music;
+    public AudioSource bellSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TimerCheck());
+        StartCoroutine(Speaker());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator TimerCheck()
+    {
+        yield return new WaitForSeconds(timeTillDeparture);
+        music.enabled = false;
+        bellSound.Play();
+    }
+    
+    public IEnumerator Speaker()
+    {
+        yield return new WaitForSeconds(speaker);
+
+    }
+}
