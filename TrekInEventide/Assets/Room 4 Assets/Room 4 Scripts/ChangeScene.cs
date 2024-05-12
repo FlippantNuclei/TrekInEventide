4ba2efde78f8b19bce74140dcdf128a5078f914d
Luke Kim <kiml803@newschool.edu>
2024-05-12 13:52:40 -0400

    Making new Script

--- TrekInEventide/Assets/Room 4 Assets/Room 4 Scripts/ChangeScene.cs
+++ TrekInEventide/Assets/Room 4 Assets/Room 4 Scripts/ChangeScene.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public int NextScene;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ChangeToScene());
     }
 
     // Update is called once per frame
@@ -15,4 +18,12 @@ public class ChangeScene : MonoBehaviour
     {
         
     }
+
+    public IEnumerator ChangeToScene()
+    {
+        yield return new WaitForSeconds(NextScene);
+        
+    }
+
+    
 }
